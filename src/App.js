039e11055d25e9ea351c032088044a5d3e9c0674
ncux <malabatalent@gmail.com>
2021-04-08 10:20:02 +0800

    1st

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="text-blue-600 text-center mt-5">
+        <h4>
+          Edit <code>src/App.js</code> and save to reload.
+        </h4>
+    </div>
+  );
+}
+
+export default App;
